c852628ce6bddc3570fdc72e76d97d4c24efe9b2
Taiki Harada <haradataiki19990927@gmail.com>
2022-02-07 04:48:57 +0000

    Add expr module

---
+++ ast/src/expr.rs
@@ -0,0 +1,31 @@
+use crate::{lit::*, op::*};
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum Expr {
+    /// A binary operation: `a + b`, "a * b"
+    Binary(ExprBinary),
+
+    /// A unary operation: `-x`
+    Unary(ExprUnary),
+
+    /// A literal in place of an expression: `1`
+    Lit(ExprLit),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct ExprBinary {
+    pub left: Box<Expr>,
+    pub op: BinOp,
+    pub right: Box<Expr>,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct ExprUnary {
+    pub op: UnOp,
+    pub expr: Box<Expr>,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct ExprLit {
+    pub lit: Lit,
+}
