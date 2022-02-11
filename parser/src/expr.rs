40c74417d99d5e5fbfb95504f0590a4f43134fd3
Taiki Harada <haradataiki19990927@gmail.com>
2022-02-11 03:03:54 +0000

    Add parser of  unary operator

--- parser/src/expr.rs
+++ parser/src/expr.rs
@@ -1,12 +1,34 @@
 use crate::Parser;
 
-use ast::lit::{self, Lit, LitInt};
+use ast::{
+    expr::*,
+    lit::{self, Lit, LitInt},
+    op::*,
+    token::Token,
+};
 
 impl Parser {
     pub fn parse_lit(&mut self) -> lit::Lit {
         let digits = self.expect_num();
 
         Lit::Int(LitInt { digits: digits })
+    }
+
+    #[allow(dead_code)]
+    fn parse_expr_unary(&mut self) -> Expr {
+        match &self.token {
+            Token::Minus => (),
+            t => panic!("Expected unary operator but got {:?}", t),
+        }
+
+        self.bump();
+
+        let expr = self.parse_lit();
+
+        Expr::Unary(ExprUnary {
+            op: UnOp::Neg,
+            expr: Box::new(Expr::Lit(ExprLit { lit: expr })),
+        })
     }
 }
 
@@ -27,6 +49,22 @@ mod tests {
             parser.parse_lit(),
             Lit::Int(LitInt {
                 digits: "10".into()
+            })
+        );
+    }
+
+    #[test]
+    fn test_parse_unary() {
+        let tokens = vec![Token::Minus, Token::Num("1".into())];
+        let mut parser = Parser::new(tokens);
+
+        assert_eq!(
+            parser.parse_expr_unary(),
+            Expr::Unary(ExprUnary {
+                op: UnOp::Neg,
+                expr: Box::new(Expr::Lit(ExprLit {
+                    lit: Lit::Int(LitInt { digits: "1".into() })
+                }))
             })
         );
     }
