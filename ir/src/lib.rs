0fe117dbe62c24cb29d01ea6ea8124d431d63f65
Taiki Harada <haradataiki19990927@gmail.com>
2022-02-14 02:34:38 +0000

    Add IR

---
+++ ir/src/lib.rs
@@ -0,0 +1,58 @@
+#[allow(dead_code)]
+#[derive(Debug, PartialEq, Clone)]
+pub struct Body {
+    stmts: Vec<Statement>,
+    local_decls: Vec<LocalDecl>,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, PartialEq, Clone)]
+pub enum Statement {
+    Assign(Box<(Place, RValue)>),
+}
+
+#[allow(dead_code)]
+#[derive(Debug, PartialEq, Clone)]
+pub enum RValue {
+    Use(Operand),
+    BinaryOp(BinOp, Box<(Operand, Operand)>),
+}
+
+#[allow(dead_code)]
+#[derive(Debug, PartialEq, Clone)]
+pub enum BinOp {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum Operand {
+    Copy(Place),
+    Constant(Box<Constant>),
+}
+
+#[allow(dead_code)]
+#[derive(Debug, PartialEq, Clone)]
+pub struct Place {
+    local: usize,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, PartialEq, Clone)]
+pub struct LocalDecl {
+    name: Option<String>,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum Constant {
+    Scalar(ScalarInt),
+}
+
+#[allow(dead_code)]
+#[derive(Debug, PartialEq, Clone)]
+pub struct ScalarInt {
+    data: u128,
+    size: u8,
+}
