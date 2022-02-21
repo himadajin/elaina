d3a4a99cbceacb6004c2cc347c236e4e14787eb8
Taiki Harada <haradataiki19990927@gmail.com>
2022-02-21 02:21:00 +0000

    Add parser of Stmt::Semi

--- parser/src/stmt.rs
+++ parser/src/stmt.rs
@@ -1,10 +1,15 @@
 use crate::Parser;
 
 use ast::stmt::*;
+use ast::token::Token;
 
 impl Parser {
     pub fn parse_stmt(&mut self) -> Stmt {
         let expr = self.parse_expr();
+
+        if self.consume(&Token::Semi) {
+            return Stmt::Semi(expr);
+        }
 
         Stmt::Expr(expr)
     }
@@ -28,8 +33,20 @@ mod tests {
     #[test]
     fn parse_expr() {
         test_stmt!(
-            "1 + 2;",
+            "1 + 2",
             stmt_expr(expr_binary(
+                expr_lit_int("1"),
+                ast::op::BinOp::Add,
+                expr_lit_int("2")
+            ))
+        );
+    }
+
+    #[test]
+    fn parse_semi() {
+        test_stmt!(
+            "1 + 2;",
+            stmt_semi(expr_binary(
                 expr_lit_int("1"),
                 ast::op::BinOp::Add,
                 expr_lit_int("2")
