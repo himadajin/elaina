9d50d16448d3b3dd69bf674a5d4c216920b8d33b
Taiki Harada <haradataiki19990927@gmail.com>
2022-02-21 03:21:45 +0000

    Add builder function of Stmt::Local

--- ast/src/builder/stmt.rs
+++ ast/src/builder/stmt.rs
@@ -1,4 +1,14 @@
-use crate::{expr::*, stmt::Stmt};
+use crate::{
+    expr::*,
+    stmt::{Local, Stmt},
+};
+
+pub fn stmt_local<T: Into<String>>(ident: T, expr: Expr) -> Stmt {
+    Stmt::Local(Local {
+        ident: ident.into(),
+        init: expr,
+    })
+}
 
 pub fn stmt_expr(expr: Expr) -> Stmt {
     Stmt::Expr(expr)
