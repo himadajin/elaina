5f4c100c297e9b5f3c48fc6c1e6f440a7c3f136b
Taiki Harada <haradataiki19990927@gmail.com>
2022-02-21 02:33:45 +0000

    Add statement of local

--- ast/src/stmt.rs
+++ ast/src/stmt.rs
@@ -2,9 +2,18 @@ use crate::expr::Expr;
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Stmt {
+    /// Local represents a let statement: `let <ident> = <expr>;`
+    Local(Local),
+
     /// Expression statement: `1 + 1`
     Expr(Expr),
 
     /// Expression statement with semicolon: `1 + 1;`
     Semi(Expr),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Local {
+    ident: String,
+    init: Expr,
 }
