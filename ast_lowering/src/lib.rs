1743abc79cd2b80b6ab0d8408e78658322957112
Taiki Harada <haradataiki19990927@gmail.com>
2022-02-24 07:03:19 +0000

    Add lower_expr_unary

--- ast_lowering/src/lib.rs
+++ ast_lowering/src/lib.rs
@@ -1,4 +1,4 @@
-use ast::{expr::*, lit::*};
+use ast::{expr::*, lit::*, op::UnOp};
 use thir;
 use ty::*;
 
@@ -19,9 +19,26 @@ impl LoweringContext {
     pub fn lower_expr(&mut self, expr: &Expr) -> thir::Expr {
         match expr {
             Expr::Binary(_) => todo!(),
-            Expr::Unary(_) => todo!(),
+            Expr::Unary(unary) => self.lower_expr_unary(&unary.op, &unary.expr),
             Expr::Lit(lit) => self.lower_expr_lit(&lit.lit),
             Expr::Ident(_) => todo!(),
+        }
+    }
+
+    fn lower_expr_unary(&mut self, op: &UnOp, expr: &Expr) -> thir::Expr {
+        match op {
+            UnOp::Neg => {
+                let thir_expr = self.lower_expr(expr);
+                let ty = ty::Ty {
+                    kind: ty::TyKind::Int(ty::IntTy::I32),
+                };
+
+                thir::Expr::Unary {
+                    op: thir::UnOp::Neg,
+                    expr: Box::new(thir_expr),
+                    ty: ty,
+                }
+            }
         }
     }
 
@@ -51,7 +68,31 @@ impl LoweringContext {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use ast::builder::*;
+    use ast::builder::expr;
+
+    #[test]
+    fn lower_expr_unary() {
+        let ast = expr::expr_unary(UnOp::Neg, expr::expr_lit_int("1"));
+        let thir = {
+            let i32_ty = ty::Ty {
+                kind: ty::TyKind::Int(ty::IntTy::I32),
+            };
+
+            let expr_lit = {
+                let lit = thir::Lit::Int(thir::LitInt { value: 1 });
+
+                thir::Expr::Lit { lit, ty: i32_ty }
+            };
+
+            thir::Expr::Unary {
+                op: thir::UnOp::Neg,
+                expr: Box::new(expr_lit),
+                ty: i32_ty,
+            }
+        };
+
+        assert_eq!(thir, LoweringContext::new().lower_expr(&ast));
+    }
 
     #[test]
     fn lower_expr_lit_int() {
