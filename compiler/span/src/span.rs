cafc80ae57e06def055c1a96eaef732a24ad64ed
Taiki Harada <haradataiki19990927@gmail.com>
2022-03-04 05:20:39 +0000

    Add span crate

---
+++ compiler/span/src/span.rs
@@ -0,0 +1,43 @@
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+pub struct SpanData {
+    pub lo: u32,
+    pub hi: u32,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Span {
+    index: u32,
+    len: u16,
+}
+
+const MAX_LEN: u32 = 0b0111_1111_1111_1111;
+
+pub const DUMMY_SP: Span = Span { index: 0, len: 0 };
+
+impl Span {
+    #[inline]
+    pub fn new(mut lo: u32, mut hi: u32) -> Self {
+        if lo > hi {
+            std::mem::swap(&mut lo, &mut hi);
+        }
+
+        let (base, len) = (lo, hi - lo);
+
+        if len <= MAX_LEN {
+            Span {
+                index: base,
+                len: len as u16,
+            }
+        } else {
+            panic!("too long span");
+        }
+    }
+
+    #[inline]
+    pub fn data(self) -> SpanData {
+        SpanData {
+            lo: self.index,
+            hi: self.index + self.len as u32,
+        }
+    }
+}
