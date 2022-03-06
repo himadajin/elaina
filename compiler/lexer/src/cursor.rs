624e10ff0696ff163823c26aec15942d9cba340f
Taiki Harada <haradataiki19990927@gmail.com>
2022-03-06 03:59:26 +0000

    Move function implementation

--- compiler/lexer/src/cursor.rs
+++ compiler/lexer/src/cursor.rs
@@ -1,3 +1,5 @@
+use crate::token::*;
+
 use std::str::Chars;
 
 pub(crate) struct Cursor<'a> {
@@ -15,7 +17,7 @@ impl<'a> Cursor<'a> {
         }
     }
 
-    pub(crate) fn first(&self) -> char {
+    fn first(&self) -> char {
         self.chars.clone().next().unwrap_or(EOF_CHAR)
     }
 
@@ -23,7 +25,7 @@ impl<'a> Cursor<'a> {
         self.chars.as_str().is_empty()
     }
 
-    pub(crate) fn len_consumed(&self) -> usize {
+    fn len_consumed(&self) -> usize {
         self.initial_len - self.chars.as_str().len()
     }
 
@@ -31,15 +33,145 @@ impl<'a> Cursor<'a> {
         self.initial_len = self.chars.as_str().len();
     }
 
-    pub(crate) fn bump(&mut self) -> Option<char> {
+    fn bump(&mut self) -> Option<char> {
         let c = self.chars.next()?;
 
         Some(c)
     }
 
-    pub(crate) fn eat_while(&mut self, mut predicate: impl FnMut(char) -> bool) {
+    fn eat_while(&mut self, mut predicate: impl FnMut(char) -> bool) {
         while predicate(self.first()) && !self.is_eof() {
             self.bump();
         }
+    }
+}
+
+pub fn is_whitespace(c: char) -> bool {
+    // This is Pattern_White_Space.
+    //
+    // Note that this set is stable (ie, it doesn't change with different
+    // Unicode versions), so it's ok to just hard-code the values.
+
+    matches!(
+        c,
+        // Usual ASCII suspects
+        '\u{0009}'   // \t
+        | '\u{000A}' // \n
+        | '\u{000B}' // vertical tab
+        | '\u{000C}' // form feed
+        | '\u{000D}' // \r
+        | '\u{0020}' // space
+
+        // NEXT LINE from latin1
+        | '\u{0085}'
+
+        // Bidi markers
+        | '\u{200E}' // LEFT-TO-RIGHT MARK
+        | '\u{200F}' // RIGHT-TO-LEFT MARK
+
+        // Dedicated whitespace characters from Unicode
+        | '\u{2028}' // LINE SEPARATOR
+        | '\u{2029}' // PARAGRAPH SEPARATOR
+    )
+}
+
+/// True if `c` is valid as a first character of an identifier.
+pub fn is_id_start(c: char) -> bool {
+    // This is XID_Start OR '_' (which formally is not a XID_Start).
+    c == '_' || unicode_xid::UnicodeXID::is_xid_start(c)
+}
+
+/// True if `c` is valid as a non-first character of an identifier.
+pub fn is_id_continue(c: char) -> bool {
+    unicode_xid::UnicodeXID::is_xid_continue(c)
+}
+
+/// The passed string is lexically an identifier.
+#[allow(dead_code)]
+pub fn is_ident(string: &str) -> bool {
+    let mut chars = string.chars();
+    if let Some(start) = chars.next() {
+        is_id_start(start) && chars.all(is_id_continue)
+    } else {
+        false
+    }
+}
+
+impl Cursor<'_> {
+    pub(crate) fn advance_token(&mut self) -> Token {
+        let first_char = self.bump().unwrap();
+        let token_kind = match first_char {
+            c if is_whitespace(c) => self.whitespace(),
+            c @ '0'..='9' => {
+                let literal_kind = self.number(c);
+                TokenKind::Literal { kind: literal_kind }
+            }
+            c if is_id_start(c) => self.ident(),
+
+            ';' => TokenKind::Semi,
+            '(' => TokenKind::OpenParen,
+            ')' => TokenKind::CloseParen,
+            '{' => TokenKind::OpenBrace,
+            '}' => TokenKind::CloseBrace,
+            '=' => TokenKind::Eq,
+            '!' => TokenKind::Bang,
+            '<' => TokenKind::Lt,
+            '>' => TokenKind::Gt,
+            '-' => TokenKind::Minus,
+            '+' => TokenKind::Plus,
+            '*' => TokenKind::Star,
+            '/' => TokenKind::Slash,
+
+            _ => TokenKind::Unknown,
+        };
+
+        Token::new(token_kind, self.len_consumed())
+    }
+
+    fn whitespace(&mut self) -> TokenKind {
+        self.eat_while(is_whitespace);
+        TokenKind::Whitespace
+    }
+
+    fn ident(&mut self) -> TokenKind {
+        self.eat_while(is_id_continue);
+        TokenKind::Ident
+    }
+
+    fn number(&mut self, first_digit: char) -> LiteralKind {
+        if first_digit == '0' {
+            let has_digits = match self.first() {
+                '0'..='9' => {
+                    self.eat_decimal_digits();
+                    true
+                }
+                _ => return LiteralKind::Int,
+            };
+
+            if !has_digits {
+                return LiteralKind::Int;
+            }
+        } else {
+            self.eat_decimal_digits();
+        }
+
+        LiteralKind::Int
+    }
+
+    fn eat_decimal_digits(&mut self) -> bool {
+        let mut has_digits = false;
+        loop {
+            match self.first() {
+                '_' => {
+                    self.bump();
+                }
+                '0'..='9' => {
+                    has_digits = true;
+                    self.bump();
+                }
+                _ => break,
+            }
+        }
+        has_digits
     }
 }
