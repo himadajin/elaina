c468c882c5c5ace3cab7a4bd712de677dd080763
Taiki Harada <haradataiki19990927@gmail.com>
2022-04-16 07:24:00 +0000

    Add some contents to hir.

--- compiler/hir/src/lib.rs
+++ compiler/hir/src/lib.rs
@@ -1,9 +1,12 @@
 pub mod def_id;
+pub mod pp;
 
 use crate::def_id::DefId;
 
 use span::symbol::Symbol;
 use ty;
+
+use std::fmt;
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Path {
@@ -78,16 +81,36 @@ pub enum Expr {
     Block { block: Box<Block> },
 
     /// Assign expression: `a = 1`
-    Assign {
-        lhs: Box<Expr>,
-        rhs: Box<Expr>,
-    },
+    Assign { lhs: Box<Expr>, rhs: Box<Expr> },
 
     /// A literal in place of an expression: `1`
     Lit { lit: Lit, ty: ty::Ty },
 
     /// A path such as variables, functions, etx: `foo`, `bar`
     Path { path: Path },
+}
+
+pub const PREC_JUMP: i8 = -30;
+
+// The range 2..=14 is reserved for AssocOp binary operator precedences.
+pub const PREC_ASSIGN: i8 = 2;
+
+pub const PREC_PREFIX: i8 = 50;
+pub const PREC_POSTFIX: i8 = 60;
+pub const PREC_PAREN: i8 = 99;
+pub const PREC_FORCE_PAREN: i8 = 100;
+
+impl Expr {
+    pub fn precedence(&self) -> i8 {
+        use Expr::*;
+        match self {
+            Break { .. } | Continue { .. } => PREC_JUMP,
+            Binary { op, .. } => op.precedence() as i8,
+            Assign { .. } => PREC_ASSIGN,
+            Unary { .. } => PREC_PREFIX,
+            Lit { .. } | Path { .. } | If { .. } | Loop { .. } | Block { .. } => PREC_PAREN,
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -102,6 +125,16 @@ pub enum Lit {
 #[derive(Debug, PartialEq, Clone)]
 pub struct LitInt {
     pub value: u128,
+}
+
+#[derive(PartialEq, Debug)]
+pub enum Fixity {
+    /// The operator is left-associative
+    Left,
+    /// The operator is right-associative
+    Right,
+    /// The operator is not-associative
+    None,
 }
 
 #[derive(Debug, PartialEq, Clone, Copy)]
@@ -135,6 +168,41 @@ pub enum BinOp {
 
     /// the `>` operator (greater than)
     Gt,
+}
+
+impl BinOp {
+    pub fn precedence(&self) -> usize {
+        use BinOp::*;
+        match *self {
+            Mul | Div => 13,
+            Add | Sub => 12,
+            Eq | Lt | Le | Ne | Ge | Gt => 7,
+        }
+    }
+
+    pub fn fixity(&self) -> Fixity {
+        use BinOp::*;
+        match *self {
+            Mul | Div | Add | Sub | Eq | Lt | Le | Ne | Ge | Gt => Fixity::Left,
+        }
+    }
+}
+
+impl fmt::Display for BinOp {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            BinOp::Mul => write!(f, "*"),
+            BinOp::Div => write!(f, "/"),
+            BinOp::Add => write!(f, "+"),
+            BinOp::Sub => write!(f, "-"),
+            BinOp::Eq => write!(f, "=="),
+            BinOp::Lt => write!(f, "<"),
+            BinOp::Le => write!(f, "<="),
+            BinOp::Ne => write!(f, "!="),
+            BinOp::Ge => write!(f, ">="),
+            BinOp::Gt => write!(f, ">"),
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Clone, Copy)]
