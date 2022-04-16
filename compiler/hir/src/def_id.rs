b87a1b1bb5e20196b9ed8f277f0cb7997df7830d
Taiki Harada <haradataiki19990927@gmail.com>
2022-04-16 06:42:05 +0000

    Implement Display trait to DefId.

--- compiler/hir/src/def_id.rs
+++ compiler/hir/src/def_id.rs
@@ -1,9 +1,17 @@
+use std::fmt;
+
 #[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
 pub struct DefId(usize);
 
 impl DefId {
     pub fn from_usize(id: usize) -> DefId {
         DefId(id)
+    }
+}
+
+impl fmt::Display for DefId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
     }
 }
 
