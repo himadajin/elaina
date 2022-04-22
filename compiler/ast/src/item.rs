ce0fa91434c0ec47d07f095744acf284dbc56a23
Taiki Harada <haradataiki19990927@gmail.com>
2022-04-22 04:56:56 +0000

    Add builder function.

--- compiler/ast/src/item.rs
+++ compiler/ast/src/item.rs
@@ -1,10 +1,28 @@
-use crate::{block::Block, ty::Ty};
+use crate::{block::Block, stmt::*, ty::Ty};
 
-use span::symbol::Ident;
+use span::symbol::{Ident, Symbol};
 
 pub struct Item {
     pub ident: Ident,
     pub kind: ItemKind,
+}
+
+impl Item {
+    pub fn fn_dummy<T: Into<Vec<Param>>, U: Into<Vec<Stmt>>>(
+        name: Symbol,
+        inputs: T,
+        output: Option<Ty>,
+        block: U,
+    ) -> Item {
+        Item {
+            ident: Ident::with_dummy_span(name),
+            kind: ItemKind::Fn(Box::new(Fn {
+                inputs: inputs.into(),
+                output,
+                body: Block::from(block),
+            })),
+        }
+    }
 }
 
 pub enum ItemKind {
@@ -20,4 +38,17 @@ pub struct Fn {
 pub struct Param {
     pub ty: Ty,
     pub ident: Ident,
+}
+
+impl Param {
+    pub fn new(ty: Ty, ident: Ident) -> Param {
+        Param { ty, ident }
+    }
+
+    pub fn new_dummy(ty: Ty, name: Symbol) -> Param {
+        Param {
+            ty,
+            ident: Ident::with_dummy_span(name),
+        }
+    }
 }
