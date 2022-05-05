a43535d56a18b4abcebac8f96fd123f28570ae06
Taiki Harada <haradataiki19990927@gmail.com>
2022-05-05 11:08:41 +0000

    Add stmt module.

--- compiler/codegen_llvm/src/lib.rs
+++ compiler/codegen_llvm/src/lib.rs
@@ -1,6 +1,7 @@
+pub mod stmt;
 pub mod value;
 
-use mir::{stmt::*, *};
+use mir::*;
 use ty::*;
 
 use anyhow;
@@ -126,85 +127,6 @@ impl<'ctx> CodegenContext<'ctx> {
                 let ret_ptr = self.local_values.first().unwrap().clone();
                 let ret_val = self.builder.build_load(ret_ptr, "").into_int_value();
                 self.builder.build_return(Some(&ret_val));
-            }
-        }
-    }
-
-    fn codegen_stmt(&self, module: &Module<'ctx>, function: FunctionValue, stmt: &Statement) {
-        match stmt {
-            Statement::Assign(stmt) => {
-                let (place, rvalue) = stmt.as_ref();
-
-                let place_ptr = self.local_values[place.local];
-
-                let value = match rvalue {
-                    RValue::Use(operand) => self.int_value(operand),
-                    RValue::BinaryOp(bin, operands) => {
-                        let (lhs, rhs) = operands.as_ref();
-                        let lhs_val = self.int_value(lhs);
-                        let rhs_val = self.int_value(rhs);
-
-                        let result = match bin {
-                            BinOp::Add => self.builder.build_int_nsw_add(lhs_val, rhs_val, ""),
-                            BinOp::Sub => self.builder.build_int_nsw_sub(lhs_val, rhs_val, ""),
-                            BinOp::Mul => self.builder.build_int_nsw_mul(lhs_val, rhs_val, ""),
-                            BinOp::Div => self.builder.build_int_signed_div(lhs_val, rhs_val, ""),
-                            BinOp::Eq => self.builder.build_int_compare(
-                                IntPredicate::EQ,
-                                lhs_val,
-                                rhs_val,
-                                "",
-                            ),
-                            BinOp::Lt => self.builder.build_int_compare(
-                                IntPredicate::SLT,
-                                lhs_val,
-                                rhs_val,
-                                "",
-                            ),
-                            BinOp::Le => self.builder.build_int_compare(
-                                IntPredicate::SLE,
-                                lhs_val,
-                                rhs_val,
-                                "",
-                            ),
-                            BinOp::Ne => self.builder.build_int_compare(
-                                IntPredicate::NE,
-                                lhs_val,
-                                rhs_val,
-                                "",
-                            ),
-                            BinOp::Ge => self.builder.build_int_compare(
-                                IntPredicate::SGE,
-                                lhs_val,
-                                rhs_val,
-                                "",
-                            ),
-                            BinOp::Gt => self.builder.build_int_compare(
-                                IntPredicate::SGT,
-                                lhs_val,
-                                rhs_val,
-                                "",
-                            ),
-                        };
-
-                        result
-                    }
-                    RValue::UnaryOp(op, operand) => {
-                        let operand_val = self.int_value(operand);
-
-                        let result = match op {
-                            UnOp::Neg => self.builder.build_int_nsw_neg(operand_val, ""),
-                        };
-
-                        result
-                    }
-                };
-
-                self.builder.build_store(place_ptr, value);
-            }
-            Statement::Println(operand) => {
-                let operand_val = self.int_value(operand);
-                self.call_buildint_print(&module, function, operand_val);
             }
         }
     }
---
+++ compiler/codegen_llvm/src/stmt.rs
@@ -0,0 +1,91 @@
+use mir::stmt::*;
+
+use inkwell::{module::Module, values::*, IntPredicate};
+
+use crate::CodegenContext;
+
+impl<'ctx> CodegenContext<'ctx> {
+    pub(crate) fn codegen_stmt(
+        &self,
+        module: &Module<'ctx>,
+        function: FunctionValue,
+        stmt: &Statement,
+    ) {
+        match stmt {
+            Statement::Assign(stmt) => {
+                let (place, rvalue) = stmt.as_ref();
+
+                let place_ptr = self.local_values[place.local];
+
+                let value = match rvalue {
+                    RValue::Use(operand) => self.int_value(operand),
+                    RValue::BinaryOp(bin, operands) => {
+                        let (lhs, rhs) = operands.as_ref();
+                        let lhs_val = self.int_value(lhs);
+                        let rhs_val = self.int_value(rhs);
+
+                        let result = match bin {
+                            BinOp::Add => self.builder.build_int_nsw_add(lhs_val, rhs_val, ""),
+                            BinOp::Sub => self.builder.build_int_nsw_sub(lhs_val, rhs_val, ""),
+                            BinOp::Mul => self.builder.build_int_nsw_mul(lhs_val, rhs_val, ""),
+                            BinOp::Div => self.builder.build_int_signed_div(lhs_val, rhs_val, ""),
+                            BinOp::Eq => self.builder.build_int_compare(
+                                IntPredicate::EQ,
+                                lhs_val,
+                                rhs_val,
+                                "",
+                            ),
+                            BinOp::Lt => self.builder.build_int_compare(
+                                IntPredicate::SLT,
+                                lhs_val,
+                                rhs_val,
+                                "",
+                            ),
+                            BinOp::Le => self.builder.build_int_compare(
+                                IntPredicate::SLE,
+                                lhs_val,
+                                rhs_val,
+                                "",
+                            ),
+                            BinOp::Ne => self.builder.build_int_compare(
+                                IntPredicate::NE,
+                                lhs_val,
+                                rhs_val,
+                                "",
+                            ),
+                            BinOp::Ge => self.builder.build_int_compare(
+                                IntPredicate::SGE,
+                                lhs_val,
+                                rhs_val,
+                                "",
+                            ),
+                            BinOp::Gt => self.builder.build_int_compare(
+                                IntPredicate::SGT,
+                                lhs_val,
+                                rhs_val,
+                                "",
+                            ),
+                        };
+
+                        result
+                    }
+                    RValue::UnaryOp(op, operand) => {
+                        let operand_val = self.int_value(operand);
+
+                        let result = match op {
+                            UnOp::Neg => self.builder.build_int_nsw_neg(operand_val, ""),
+                        };
+
+                        result
+                    }
+                };
+
+                self.builder.build_store(place_ptr, value);
+            }
+            Statement::Println(operand) => {
+                let operand_val = self.int_value(operand);
+                self.call_buildint_print(&module, function, operand_val);
+            }
+        }
+    }
+}
